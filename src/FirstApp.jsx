ebe7c2e115d5b925db66921ecdfaaee08f180078
rosa-uwur <rsmaldonadov@gmail.com>
2024-11-20 17:25:36 -0600

    uwu2

---
+++ src/FirstApp.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+
+//creacion de un fragmento
+export function FirstApp(){
+
+    return (  
+        // aqui se crea el fragmento
+        <>
+        <h2>First app</h2>
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {App} from './HelloWorldApp'
+import { FirstApp } from './FirstApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <FirstApp />
   </React.StrictMode>
 );
